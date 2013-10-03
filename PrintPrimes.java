7e5fabcc6ad7e50e6c7c282b12b26dbc99258acd
Vanessa Jones <vanessa.jones2@mail.mcgill.ca>
2013-10-03 17:10:45 -0400

    Added comment to the printPrimes() method

--- PrintPrimes.java
+++ PrintPrimes.java
@@ -38,7 +38,7 @@ public class PrintPrimes {
       boolean isPrime;
       int N;
       int MULT[] = new int[ORDMAX + 1];
-
+      
       int currentOddNumber = 1;
       int ORD = 2;
       int SQUARE = 9;
@@ -64,24 +64,37 @@ public class PrintPrimes {
         listOfPrimes[primesFoundSoFar] = currentOddNumber;
       }
     }
-
+  
+  /* Method printPrimes that prints the prime numbers found in calculatePrimes() */
     public void printPrimes() {
       
-        int pageNumber = 1;
-        int pageOffset = 1;
+      //Initilaze local variables
+      int pageNumber = 1;
+      int pageOffset = 1;
         
+        // While loop to print the title of the page at every new page
         while (pageOffset <= numberOfPrimes) {
           
           System.out.println("The First " + numberOfPrimes +
                                " Prime Numbers --- Page " + pageNumber);
           System.out.println("");
           
+          
+          /* Loop to print the array listOfPrime[] on each page
+           * The columns of every row are printed first before printing the next row */
+          
+          // for every row
           for (int rowIndex = pageOffset; rowIndex < (pageOffset + rowsPerPage); rowIndex++){
+            // for every column
             for (int columnIndex = 0; columnIndex < columnsPerPage; columnIndex++)
+              // print only the adequate number of prime 
               if (rowIndex + (columnIndex * rowsPerPage) <= numberOfPrimes)
+                // print the prime at the index
                 System.out.format("%10d", listOfPrimes[rowIndex + (columnIndex * rowsPerPage)]);
             System.out.println("");
           }
+          
+          // Change the page, update pageNumber and pageOffset
           System.out.println("\f");
           pageNumber = pageNumber + 1;
           pageOffset = pageOffset + rowsPerPage* columnsPerPage;
